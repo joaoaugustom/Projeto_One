e8453e04d8733df6f654959c1ace1e4e6a9b22a8
joaoaugustom <joaoaugusto.goncalves@hotmail.com>
2020-03-15 19:51:30 -0300

    Teste de Banco de dados

--- Teste_1.py
+++ Teste_1.py
@@ -1,5 +1,13 @@
-print('Hello World')
-a = 2
-if a == 2:
-    print(a)
+import sqlite3
 
+conexao = sqlite3.connect("agenda.db")
+cursor = conexao.cursor()
+cursor.execute('create table agenda( nome text, telefone text)')
+cursor.execute('insert into agenda (nome, telefone) values(?, ?)', ("Nilo", "7788-1432"))
+conexao.commit()
+cursor.execute("select * from agenda")
+impressao = cursor.fetchall()
+for a in impressao:
+    print(f"Nome: {a[0]}\nTelefone: {a[1]}")
+cursor.close()
+conexao.close()
